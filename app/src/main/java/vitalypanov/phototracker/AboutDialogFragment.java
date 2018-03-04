6efc4a9ffbb7652e3e797bcbb80874b8fc2857f2
panov <panovvitalik@gmail.com>
2018-03-04 11:35:39 +0300

    About dialog was implemented. and some other gui changes...

--- app/src/main/java/vitalypanov/phototracker/AboutDialogFragment.java
+++ app/src/main/java/vitalypanov/phototracker/AboutDialogFragment.java
@@ -1,11 +1,16 @@
 package vitalypanov.phototracker;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -14,8 +19,10 @@ import android.view.ViewGroup;
 
 public class AboutDialogFragment extends Fragment {
     private static final String TAG = "PhotoTracker";
+    private TextView mAppVersionView;
 
     public static AboutDialogFragment newInstance() {
+
         return new AboutDialogFragment();
     }
 
@@ -23,8 +30,42 @@ public class AboutDialogFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        // show app version
+        mAppVersionView = (TextView)view.findViewById(R.id.app_version_view);
+        mAppVersionView.setText(getResources().getText(R.string.app_version_title) + " " + getAppVersion());
+
+        // change title and show back button
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.track_toolbar);
+        AppCompatActivity parentActivity =(AppCompatActivity)getActivity();
+        parentActivity.setSupportActionBar(toolbar);
+        parentActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        parentActivity.getSupportActionBar().setTitle(R.string.app_about_title);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+
         return view;
     }
+
+    /**
+     * Getting app version text
+     * @return
+     */
+    private String getAppVersion(){
+        String versionText = "";
+        try {
+            PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            versionText =pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionText;
+    }
+
 
     /**
      * Show About dialog
